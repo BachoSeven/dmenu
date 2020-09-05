eb5cfb57a1c356a3361af5233aaf598a0b94b345
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-05 18:54:22 +0200

    bigger font

--- config.def.h
+++ config.def.h
@@ -3,11 +3,12 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */    static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int min_width = 500;                    /* minimum width when centered */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=16",
-	"Mutant Standard emoji:pixelsize=16:antialias=true:autohint=true"
+	"Mutant Standard emoji:pixelsize=20:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
