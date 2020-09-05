a3ad3f7caf7c55aa174cc64a817094e43b3ee8c7
Francesco Minnocci <ad17fmin@uwcad.it>
2020-09-05 18:55:39 +0200

    bigger font

--- config.def.h
+++ config.def.h
@@ -8,7 +8,7 @@ static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't us
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=16",
-	"Mutant Standard emoji:pixelsize=22:antialias=true:autohint=true"
+	"Mutant Standard emoji:pixelsize=20:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
