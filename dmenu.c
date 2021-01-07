b4534a6805b6b6fb9cd00c0d0f0fb9af6d9a5906
Francesco Minnocci <ad17fmin@uwcad.it>
2021-01-07 17:55:58 +0100

    Update documentation for Ctrl+v

--- dmenu.c
+++ dmenu.c
@@ -523,7 +523,7 @@ keypress(XKeyEvent *ev)
 			while (cursor > 0 && !strchr(worddelimiters, text[nextrune(-1)]))
 				insert(NULL, nextrune(-1) - cursor);
 			break;
-		case XK_v: /* paste selection */
+		case XK_v: /* paste selection (ctrl+v, default was ctrl+y) */
 		case XK_Y:
 			XConvertSelection(dpy, (ev->state & ShiftMask) ? clip : XA_PRIMARY,
 			                  utf8, utf8, win, CurrentTime);
